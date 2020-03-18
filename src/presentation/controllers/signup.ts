0da5158cec22835fda5fbd0ddf15fdd3a8377a11
mateus.anjos <mateus.anjos@finnet.com.br>
2020-03-17 21:53:45 -0300

    feat: throw error if no email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,14 @@
 export class SignUpController {
   handler (httpRequest: any): any {
+    let body = null
+    if (!httpRequest.body.name) {
+      body = new Error('Missing param: name')
+    } else {
+      body = new Error('Missing param: email')
+    }
     return {
       statusCode: 400,
-      body: new Error('Missing param: name')
+      body: body
     }
   }
 }
