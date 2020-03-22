53973bb84f6a7a0068854915cc44917b1e0b12c7
mateus.anjos <mateus.anjos@finnet.com.br>
2020-03-22 20:57:37 -0300

    feat: ensure emailValidator Adapter calls validator with correct email

--- tests/Utils/email-validator-adapter.spec.ts
+++ tests/Utils/email-validator-adapter.spec.ts
@@ -11,7 +11,7 @@ describe('EmailValidator Adapter', () => {
   test('Should return false if validator fail', () => {
     const sut = new EmailValidatorAdapter()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
-    const isValid = sut.isValid('valid_email@mail.com')
+    const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBe(false)
   })
   test('Should return true if validator success', () => {
@@ -21,8 +21,8 @@ describe('EmailValidator Adapter', () => {
   })
   test('Should call EmailValidatorAdapter with correct values', () => {
     const sut = new EmailValidatorAdapter()
-    const isValidSpy = jest.spyOn(sut, 'isValid')
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
     sut.isValid('any_email@email.com')
-    expect(isValidSpy).toHaveBeenCalledWith('any_email@email.com')
+    expect(isEmailSpy).toHaveBeenCalledWith('any_email@email.com')
   })
 })
