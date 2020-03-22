fe94d111132297e2b2d70c1d993299b74f927255
mateus.anjos <mateus.anjos@finnet.com.br>
2020-03-22 00:34:54 -0300

    chore: add jest configs and scripts

--- jest-unit-config.js
+++ jest-unit-config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
   coverageReporters: ['json', 'text'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
-  }
+  },
+  testMatch: ['<rootDir>/tests/**/*.spec.ts']
 }
