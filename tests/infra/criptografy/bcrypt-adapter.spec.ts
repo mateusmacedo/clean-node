0975b783ca4c7d8a2f59f1514021ba0f9298d7e9
mateus.anjos <mateus.anjos@finnet.com.br>
2020-03-24 01:57:18 -0300

    test: ensure BcryptAdapter return throw if bcrypt throw

--- tests/infra/criptografy/bcrypt-adapter.spec.ts
+++ tests/infra/criptografy/bcrypt-adapter.spec.ts
@@ -24,4 +24,11 @@ describe('Bcrypt Adapter', () => {
     const hashedValue = await sut.encrypt('any_value')
     expect(hashedValue).toBe('hashed_value')
   })
+  test('Should throw if bcryṕt throws ', async () => {
+    const sut = makeSut()
+    jest.spyOn(sut, 'encrypt')
+      .mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
+  })
 })
