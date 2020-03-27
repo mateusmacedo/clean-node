8e6a8ac807eb2c59288e6d081c9dded9f89a5f2c
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2020-03-27 19:36:15 -0300

    refactor: clear database between tests and npm fix audit

--- tests/infra/db/mongodb/repositories/account.spec.ts
+++ tests/infra/db/mongodb/repositories/account.spec.ts
@@ -8,8 +8,18 @@ describe('Accounts Mongo Repository', () => {
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  const makeSut = (): AccountMongoRepository => {
+    return new AccountMongoRepository()
+  }
+
   test('Should return an account on success', async () => {
-    const sut = new AccountMongoRepository()
+    const sut = makeSut()
     const account = await sut.add({
       name: 'any_name',
       email: 'any_email@email.com',
