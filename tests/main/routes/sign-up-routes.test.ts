bf46e7fbab96110a4899197d22c04f7d568391e9
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2020-03-28 00:56:41 -0300

    test: sign up route with router config not working

---
+++ tests/main/routes/sign-up-routes.test.ts
@@ -0,0 +1,16 @@
+import request from 'supertest'
+import app from '../../../src/main/config/app'
+
+describe('SignUp Routes', () => {
+  test('Should return an account on success', async () => {
+    await request(app)
+      .post('api/signup')
+      .send({
+        name: 'Mateus',
+        email: 'macedodosanjosmateus@gmail.com',
+        password: 'secret*123',
+        passwordConfirmation: 'secret*123'
+      })
+      .expect(200)
+  })
+})
