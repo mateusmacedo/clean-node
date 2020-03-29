5a489dad1522ac1e6bea143f10062bb7ee264ab1
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2020-03-28 23:07:04 -0300

    test: add sign up routes integration test

--- tests/main/routes/sign-up-routes.test.ts
+++ tests/main/routes/sign-up-routes.test.ts
@@ -4,13 +4,12 @@ import app from '../../../src/main/config/app'
 describe('SignUp Routes', () => {
   test('Should return an account on success', async () => {
     await request(app)
-      .post('api/signup')
+      .post('/api/signup')
       .send({
         name: 'Mateus',
         email: 'macedodosanjosmateus@gmail.com',
         password: 'secret*123',
         passwordConfirmation: 'secret*123'
-      })
-      .expect(200)
+      }).expect(200)
   })
 })
