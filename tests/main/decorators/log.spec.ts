cef1debf743a10e49fdad0afe95356ca9ed0486e
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2020-04-11 18:56:59 -0300

    refactor: move sut and deps creation to factories

--- tests/main/decorators/log.spec.ts
+++ tests/main/decorators/log.spec.ts
@@ -1,25 +1,36 @@
 import { LogControllerDecorator } from '../../../src/main/decorators/log-controller-decorator'
 import { Controller, HttpRequest, HttpResponse } from '../../../src/presentation/protocols'
 
+interface SutTypes {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handler (httpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpResponse: HttpResponse = {
+        statusCode: 200,
+        body: {
+          id: 'any_value',
+          name: 'any_value',
+          email: 'any_value@mail.com,',
+          password: 'any_value'
+        }
+      }
+      return new Promise(resolve => resolve(httpResponse))
+    }
+  }
+  return new ControllerStub()
+}
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController()
+  const logControllerDecorator = new LogControllerDecorator(controllerStub)
+  return { sut: logControllerDecorator, controllerStub }
+}
 describe('Log Decorator', () => {
   test('Should call controller handle with correct values', async () => {
-    class ControllerStub implements Controller {
-      async handler (httpRequest: HttpRequest): Promise<HttpResponse> {
-        const httpResponse: HttpResponse = {
-          statusCode: 200,
-          body: {
-            id: 'any_value',
-            name: 'any_value',
-            email: 'any_value@mail.com,',
-            password: 'any_value'
-          }
-        }
-        return new Promise(resolve => resolve(httpResponse))
-      }
-    }
-
-    const controllerStub = new ControllerStub()
-    const sut = new LogControllerDecorator(controllerStub)
+    const { sut, controllerStub } = makeSut()
     const handleSpy = jest.spyOn(controllerStub, 'handler')
     const httpRequest = {
       body: {
