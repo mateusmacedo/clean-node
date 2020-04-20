905b6e070b706b128e85b6a635ce1f25928bf426
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2020-04-19 22:52:15 -0300

    feat: ensure DbAuthentication calls LoadAccountByEmailRepoisitory with correct values

---
+++ src/data/usecases/authentication/db-authentication.ts
@@ -0,0 +1,15 @@
+import { Authentication, AuthenticationModel } from '../../../domain/usercases'
+import { LoadAccountByEmailRepository } from '../../protocols/repositories/load-account-by-email-repository'
+
+export class DbAuthentication implements Authentication {
+  private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+
+  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository) {
+    this.loadAccountByEmailRepository = loadAccountByEmailRepository
+  }
+
+  async auth (authenticationModel: AuthenticationModel): Promise<string> {
+    await this.loadAccountByEmailRepository.load(authenticationModel.email)
+    return new Promise(resolve => resolve('any_value'))
+  }
+}
